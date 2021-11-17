ddf8e6c2a377539327302aebde11f45839792b1d
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2021-11-16 20:25:10 -0500

    adding middleware

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
+// server setup
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./public"));
+
