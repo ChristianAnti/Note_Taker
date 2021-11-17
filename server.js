9cefc4d9379a0849af2b4ee931e8203dbf989288
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2021-11-16 20:37:52 -0500

    added first request

--- server.js
+++ server.js
@@ -4,12 +4,38 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
+
 // server setup
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 // middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./public"));
 
+
+// the GET request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./db/db.json", "utf8").then(function(data)  {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+
+// the POST request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./db/db.json", "utf8").then(function(data)  {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
+
+// the DELETE request
+app.get("/api/notes", function(req, res) {
+    readFileAsync("./db/db.json", "utf8").then(function(data)  {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+    })
+});
